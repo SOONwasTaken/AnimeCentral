31422e8b4dd7622b834a92843706b84dd5616b1f
SOONwasTaken <GoldenGamer457@gmail.com>
2025-05-25 19:30:58 -0400

    test module

---
+++ main.js
@@ -0,0 +1,41 @@
+const repoName = '/AnimeCentral/';
+
+const fs = require('fs');
+export const isGithubPages = fs.existsSync(repoName);
+export const baseURL = isGithubPages ? './' : repoName;
+
+function makeHttpObject() {
+    try { return new XMLHttpRequest(); }
+    catch (e) { }
+    try { return new ActiveXObject("Msxml2.XMLHTTP"); }
+    catch (e) { }
+    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+    catch (e) { }
+
+    throw new Error("Could not create HTTP request object.");
+}
+
+export function createLayout(containerId) {
+    const linkTags = document.getElementsByTagName('link');
+    for (const tag of linkTags) {
+        tag.setAttribute('href', baseURL + tag.href);
+    }
+
+    const containerElement = document.getElementById('#' + containerId);
+    const anchorTags = document.getElementsByClassName('directory-link');
+
+    for (const tag of anchorTags) {
+        tag.setAttribute('href', baseURL + tag.href);
+
+        tag.addEventListener('click', function (e) {
+            e.preventDefault();
+            const request = makeHttpObject();
+            request.open("GET", tag.href, true);
+            request.send(null);
+            request.onreadystatechange = function () {
+                if (request.readyState === 4)
+                    containerElement.innerHTML = request.responseText;
+            };
+        });
+    }
+};
--- utils.js
+++
@@ -1,4 +0,0 @@
-export const GITHUB_PAGES_DIRECTORY = '/AnimeCentral/';
-export const IS_ON_GITHUB_PAGES = await import('fs').then((fs) => {
-    return fs.existsSync(GITHUB_PAGES_DIRECTORY);
-});
